f504a89d817dd43f668d81cce37262e5d585d6cc
narender arnote <narender.techsparksit@gmail.com>
2018-05-12 12:39:26 +0530

    'first-commit'

---
+++ app/Http/Controllers/ShopifyController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Shopify;
+class ShopifyController extends Controller
+{
+    protected $shop = "example.myshopify.com";
+	protected $foo;
+	protected $scopes = ['read_products','read_themes'];
+	  
+	public function getPermission(){
+
+	    $this->foo = Shopify::make($this->shop, $this->scope);
+
+	    return $this->foo->redirect();
+	  
+	}
+	  
+	public function getResponse(Request $request){
+	    
+	    $this->getPermission();
+	    
+	    // Get user data, you can store it in the data base
+	    $user = $this->foo->auth()->getUser();
+	    
+	    //GET request to products.json
+	    return $this->foo->auth()->get('products.json', ['fields'=>'id,images,title']);
+	}
+}
